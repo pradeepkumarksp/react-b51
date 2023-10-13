85c8c443356177982229b4c454707ec8da9716b2
pradeepkumarksp <mycomputer252545@gmail.com>
2023-10-13 19:54:37 +0530

    create multiple compnents

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+
+const Hello = (props)=>{
+
+  console.log(props)
+  return(
+    <div>
+      <p>
+        hello, yourname is { props.username} and your age is {props.age}.
+      </p>
+    </div>
+  )
+}
+/*
+-this called component (function = component)
+*/
+
 function App(){
+  
+  const name1 = 'pradeep'
+  const name2 = 'srikanth'
+  const age1 = 28
+  const age2 = 27
+
   return(
     <div>
     Hello world!
+
+    <Hello username = {name1} age={age1}/>
+    <Hello username = {name2} age={age2}/>
     </div>
   )
 }
